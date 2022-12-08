ecd62d163412806b4b8bb4c2ddc52e7687a196f4
orientacao1234 <115122347+orientacao1234@users.noreply.github.com>
2022-12-08 11:44:59 -0300

    Add files via upload

---
+++ C79_Template-main/main.js
@@ -0,0 +1,29 @@
+menuListArray = ["Pizza Vegetariana","pizza de frango", "pizza portuguesa", "pizza quatro queijos", "pizza de calabresa", "pizza extravaganza" ];
+
+function getMenu(){
+var htmldata = "";
+menuListArray.sort();
+for (var i = 0; i < menuListArray.length; i++) {
+    htmldata = htmldata+menuListArray[i]+'<br>'
+}
+document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+function addItem(){
+var htmldata;
+var imgtags = '<img id = "im1" src="images/pizzaImg.png" />';
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata = "";
+for (var i = 0; i < menuListArray.length; i++) {
+    htmldata = htmldata+imgtags+menuListArray[i]+'<br>'
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+
+}
+
+function addTop(){
+var item = document.getElementsById("addItem").value;
+menuListArray.push(item);
+addItem();
+}
